52ad432a6cc61295e6e2917eda6f99f88579e51c
Camila Balestrin <camila.balestrin@hpe.com>
2016-06-21 18:35:17 -0300

    Add license to files developed by Chakravarthy Racharla

--- dependencies/python-hpICsp/examples/Backup_Content/Backup_Content.py
+++ dependencies/python-hpICsp/examples/Backup_Content/Backup_Content.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
+###
+# (C) Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###
 
 import hpICsp
 import argparse
@@ -34,20 +55,20 @@ def main():
 	#Login using parsed login information
 	credential = {'userName': applianceUser, 'password': appliancePassword}
 	con.login(credential)
-	
-	#Export appliance content 
+
+	#Export appliance content
 	bytesContent=st.export_content()
-	
+
 	#Write content to zip file containing current time.
 	name=("Backup " + time.strftime("%c") + ".zip")
 	newFile = open (name, "wb")
 	newFile.write(bytesContent)
 	os.chmod(name, stat.S_IRWXO | stat.S_IRWXG | stat.S_IRWXU)
 	newFile.close()
-	
+
 	#Logout of appliance
 	con.logout()
-		
+
 if __name__ == '__main__':
 	import sys
 	sys.exit(main())
