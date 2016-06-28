03f887e73b21ceeba993fd86beab0b4b74996b7f
Camila Balestrin <camila.balestrin@hpe.com>
2016-06-28 15:36:44 -0300

    Add module interconnect type facts

---
+++ library/oneview_interconnect_type_facts.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+from ansible.module_utils.basic import *
+from hpOneView.oneview_client import OneViewClient
+
+DOCUMENTATION = '''
+---
+module: oneview_interconnect_type_facts
+short_description: Retrieve facts about one or more of the OneView Interconnect Types.
+description:
+    - Retrieve facts about one or more of the Interconnect Types from OneView.
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView"
+author: "Camila Balestrin (@balestrinc)"
+options:
+    config:
+      description:
+        - Path to a .json configuration file containing the OneView client configuration.
+      required: true
+    name:
+      description:
+        - Interconnect Type name.
+      required: false
+notes:
+    - "A sample configuration file for the config parameter can be found at:
+      https://github.hpe.com/Rainforest/oneview-ansible/blob/master/examples/oneview_config.json"
+'''
+
+EXAMPLES = '''
+- name: Gather facts about all Interconnect Types
+  oneview_interconnect_type_facts:
+    config: "{{ config_file_path }}"
+
+- debug: var=interconnect_types
+
+- name: Gather facts about a Interconnect Type by name
+  oneview_interconnect_type_facts:
+    config: "{{ config_file_path }}"
+    name: HP VC Flex-10 Enet Module
+
+- debug: var=interconnect_types
+'''
+
+RETURN = '''
+interconnect_types:
+    description: Has all the OneView facts about the Interconnect Types.
+    returned: always, but can be null
+    type: complex
+'''
+
+
+class InterconnectTypeFactsModule(object):
+    argument_spec = dict(
+        config=dict(required=True, type='str'),
+        name=dict(required=False, type='str')
+    )
+
+    def __init__(self):
+        self.module = AnsibleModule(argument_spec=self.argument_spec,
+                                    supports_check_mode=False)
+        self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+
+    def run(self):
+        try:
+            if self.module.params['name']:
+                self.__get_by_name(self.module.params['name'])
+            else:
+                self.__get_all()
+
+        except Exception as exception:
+            self.module.fail_json(msg=exception.message)
+
+    def __get_by_name(self, name):
+        interconnect_types = self.oneview_client.interconnect_types.get_by('name', name)
+
+        self.module.exit_json(changed=False,
+                              ansible_facts=dict(interconnect_types=interconnect_types))
+
+    def __get_all(self):
+        interconnect_types = self.oneview_client.interconnect_types.get_all()
+
+        self.module.exit_json(changed=False,
+                              ansible_facts=dict(interconnect_types=interconnect_types))
+
+
+def main():
+    InterconnectTypeFactsModule().run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_interconnect_type_facts.py
@@ -0,0 +1,124 @@
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+import unittest
+import mock
+
+from hpOneView.oneview_client import OneViewClient
+from oneview_interconnect_type_facts import InterconnectTypeFactsModule
+
+ERROR_MSG = 'Fake message error'
+
+PARAMS_GET_ALL = dict(
+    config='config.json',
+    name=None
+)
+
+PARAMS_GET_BY_NAME = dict(
+    config='config.json',
+    name="HP VC Flex-10 Enet Module"
+)
+
+PRESENT_TYPES = [{
+    "name": "HP VC Flex-10 Enet Module",
+    "uri": "/rest/interconnect-types/e6d938ac-0588-44c9-95f2-610f3da4a941"
+}]
+
+
+def create_ansible_mock(params):
+    mock_params = mock.Mock()
+    mock_params.__getitem__ = mock.Mock(side_effect=lambda name: params[name])
+
+    mock_ansible = mock.Mock()
+    mock_ansible.params = mock_params
+    return mock_ansible
+
+
+class InterconnectTypeFactsSpec(unittest.TestCase):
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_interconnect_type_facts.AnsibleModule')
+    def test_should_get_all_interconnect_types(self, mock_ansible_module,
+                                               mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.interconnect_types.get_all.return_value = PRESENT_TYPES
+
+        mock_ov_client_from_json_file.return_value = mock_ov_instance
+
+        mock_ansible_instance = create_ansible_mock(PARAMS_GET_ALL)
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        InterconnectTypeFactsModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(interconnect_types=(PRESENT_TYPES))
+        )
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_interconnect_type_facts.AnsibleModule')
+    def test_should_fail_when_get_all_raises_error(self, mock_ansible_module,
+                                                   mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.interconnect_types.get_all.side_effect = Exception(ERROR_MSG)
+
+        mock_ov_client_from_json_file.return_value = mock_ov_instance
+
+        mock_ansible_instance = create_ansible_mock(PARAMS_GET_ALL)
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        InterconnectTypeFactsModule().run()
+
+        mock_ansible_instance.fail_json.assert_called_once()
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_interconnect_type_facts.AnsibleModule')
+    def test_should_get_interconnect_type_by_name(self, mock_ansible_module,
+                                                  mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.interconnect_types.get_by.return_value = PRESENT_TYPES
+
+        mock_ov_client_from_json_file.return_value = mock_ov_instance
+
+        mock_ansible_instance = create_ansible_mock(PARAMS_GET_BY_NAME)
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        InterconnectTypeFactsModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(interconnect_types=(PRESENT_TYPES))
+        )
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_interconnect_type_facts.AnsibleModule')
+    def test_should_fail_when_get_by_name_raises_error(self,
+                                                       mock_ansible_module,
+                                                       mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.interconnect_types.get_by.side_effect = Exception(ERROR_MSG)
+
+        mock_ov_client_from_json_file.return_value = mock_ov_instance
+
+        mock_ansible_instance = create_ansible_mock(PARAMS_GET_BY_NAME)
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        InterconnectTypeFactsModule().run()
+
+        mock_ansible_instance.fail_json.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
