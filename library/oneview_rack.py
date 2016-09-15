c3636e261c4629bf159746d31633f54657eafd82
Camila Balestrin <camila.balestrin@hpe.com>
2016-09-15 17:48:33 -0300

    Add module oneview_rack

---
+++ library/oneview_rack.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+from ansible.module_utils.basic import *
+from hpOneView.oneview_client import OneViewClient
+from hpOneView.common import resource_compare
+
+DOCUMENTATION = '''
+---
+module: oneview_rack
+short_description: Manage OneView Racks resources.
+description:
+    - Provides an interface to manage Rack resources. Can create, update, delete.
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView"
+author: "Camila Balestrin (@balestrinc)"
+options:
+    config:
+      description:
+        - Path to a .json configuration file containing the OneView client configuration.
+      required: true
+    state:
+        description:
+            - Indicates the desired state for the Rack resource.
+              'present' will ensure data properties are compliant to OneView. To change the name of the Rack,
+               a 'newName' in the data must be provided.
+              'absent' will remove the resource from OneView, if it exists.
+        choices: ['present', 'absent']
+    data:
+      description:
+        - List with the Rack properties.
+      required: true
+notes:
+    - "A sample configuration file for the config parameter can be found at:
+       https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
+'''
+
+EXAMPLES = '''
+- name: Ensure that a Rack is present using the default configuration
+  oneview_rack:
+    config: "{{ config_file_path }}"
+    state: present
+    data:
+      name: 'Rack Name'
+
+- name: Add rack with custom size and a single mounted enclosure at slot 20
+  oneview_rack:
+    config: "{{ config_file_path }}"
+    state: present
+    data:
+      name: 'Rack101'
+      depth: 1500
+      height: 2500
+      width: 1200
+      rackMounts:
+        - mountUri: "/rest/enclosures/39SGH102X6J2"
+          topUSlot: 20
+          uHeight: 10
+
+- name: Rename the Rack to 'Rack101'
+  oneview_rack:
+    config: "{{ config_file_path }}"
+    state: present
+    data:
+      name: 'Rack Name'
+      newName: 'Rack101'
+
+- name: Ensure that Rack is absent
+  oneview_rack:
+    config: "{{ config_file_path }}"
+    state: absent
+    data:
+      name: 'Rack Name'
+'''
+
+RETURN = '''
+rack:
+    description: Has the facts about the OneView Racks.
+    returned: on state 'present'. Can be null.
+    type: complex
+'''
+
+RACK_CREATED = 'Rack created successfully.'
+RACK_UPDATED = 'Rack updated successfully.'
+RACK_DELETED = 'Rack deleted successfully.'
+RACK_ALREADY_EXIST = 'Rack already exists.'
+RACK_ALREADY_ABSENT = 'Nothing to do.'
+RACK_NOT_FOUND = 'Rack was not found for this operation.'
+RACK_NEW_NAME_ALERADY_EXISTS = 'Rack with new name already exists.'
+
+
+class RackModule(object):
+    argument_spec = dict(
+        config=dict(required=True, type='str'),
+        state=dict(
+            required=True,
+            choices=['present', 'absent']
+        ),
+        data=dict(required=True, type='dict')
+    )
+
+    def __init__(self):
+        self.module = AnsibleModule(argument_spec=self.argument_spec, supports_check_mode=False)
+        self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+
+    def run(self):
+        state = self.module.params['state']
+        data = self.module.params['data']
+
+        try:
+            facts = {}
+            if state == 'present':
+                changed, msg, resource = self.__present(data)
+                facts = dict(ansible_facts=dict(rack=resource))
+            elif state == 'absent':
+                changed, msg = self.__absent(data)
+
+            self.module.exit_json(changed=changed, msg=msg, **facts)
+
+        except Exception as exception:
+            self.module.fail_json(msg=exception.message)
+
+    def __present(self, data):
+        resource = self.__get_by_name(data['name'])
+
+        self.__check_rename(data, resource)
+
+        if not resource:
+            return True, RACK_CREATED, self.oneview_client.racks.add(data)
+        else:
+            return self.__update(data, resource)
+
+    def __absent(self, data):
+        resource = self.__get_by_name(data['name'])
+
+        changed = False
+        msg = RACK_ALREADY_ABSENT
+
+        if resource:
+            self.oneview_client.racks.remove(resource)
+            changed = True
+            msg = RACK_DELETED
+
+        return changed, msg
+
+    def __update(self, data, resource):
+        merged_data = resource.copy()
+        merged_data.update(data)
+
+        changed = False
+        msg = RACK_ALREADY_EXIST
+
+        if not resource_compare(resource, merged_data):
+            resource = self.oneview_client.racks.update(merged_data)
+            changed = True
+            msg = RACK_UPDATED
+
+        return changed, msg, resource
+
+    def __check_rename(self, data, resource):
+
+        if "newName" not in data:
+            return
+
+        resource_new_name = self.__get_by_name(data['newName'])
+
+        if not resource:
+            self.module.exit_json(changed=False, msg=RACK_NOT_FOUND)
+        elif resource_new_name:
+            self.module.exit_json(changed=False, msg=RACK_NEW_NAME_ALERADY_EXISTS)
+
+        data["name"] = data.pop("newName")
+
+    def __get_by_name(self, name):
+        result = self.oneview_client.racks.get_by('name', name)
+        return result[0] if result else None
+
+
+def main():
+    RackModule().run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_oneview_rack.py
@@ -0,0 +1,261 @@
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+import unittest
+import mock
+
+from hpOneView.oneview_client import OneViewClient
+from oneview_rack import RackModule
+from oneview_rack import RACK_CREATED, RACK_ALREADY_EXIST, RACK_UPDATED
+from oneview_rack import RACK_DELETED, RACK_ALREADY_ABSENT, RACK_NOT_FOUND, RACK_NEW_NAME_ALERADY_EXISTS
+from copy import deepcopy
+
+FAKE_MSG_ERROR = 'Fake message error'
+
+DEFAULT_RACK_TEMPLATE = dict(
+    name='New Rack 2',
+    autoLoginRedistribution=True,
+    fabricType='FabricAttach'
+)
+
+PARAMS_FOR_PRESENT = dict(
+    config='config.json',
+    state='present',
+    data=dict(name=DEFAULT_RACK_TEMPLATE['name'])
+)
+
+PARAMS_FOR_ABSENT = dict(
+    config='config.json',
+    state='absent',
+    data=dict(name=DEFAULT_RACK_TEMPLATE['name'])
+)
+
+
+def create_ansible_mock(params):
+    mock_params = mock.Mock()
+    mock_params.__getitem__ = mock.Mock(side_effect=lambda name: params[name])
+
+    mock_ansible = mock.Mock()
+    mock_ansible.params = mock_params
+    return mock_ansible
+
+
+class RackPresentStateSpec(unittest.TestCase):
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_create_new_rack(self, mock_rack_module, mock_from_json):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = []
+        mock_ov_instance.racks.add.return_value = DEFAULT_RACK_TEMPLATE
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_PRESENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        RackModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            msg=RACK_CREATED,
+            ansible_facts=dict(rack=DEFAULT_RACK_TEMPLATE)
+        )
+
+    @mock.patch('oneview_rack.resource_compare')
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_not_update_when_data_is_equals(self, mock_rack_module, mock_from_json_file, mock_resource_compare):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+
+        mock_from_json_file.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_PRESENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        mock_resource_compare.return_value = True
+
+        RackModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            msg=RACK_ALREADY_EXIST,
+            ansible_facts=dict(rack=DEFAULT_RACK_TEMPLATE)
+        )
+
+    @mock.patch('oneview_rack.resource_compare')
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_update_when_data_has_modified_attributes(self, mock_rack_module, mock_from_json, mock_resource_compare):
+        mock_resource_compare.return_value = False
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+        mock_ov_instance.racks.update.return_value = DEFAULT_RACK_TEMPLATE
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_PRESENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        RackModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            msg=RACK_UPDATED,
+            ansible_facts=dict(rack=DEFAULT_RACK_TEMPLATE)
+        )
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_warning_rename_when_rack_not_exist(self, mock_rack_module, mock_from_json):
+        params_for_present = deepcopy(PARAMS_FOR_PRESENT)
+        params_for_present['data']['newName'] = "Rename the Rack"
+
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = []
+
+        mock_ov_instance.racks.add.return_value = DEFAULT_RACK_TEMPLATE
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(params_for_present)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        RackModule().run()
+
+        exit_json_calls = [
+            mock.call(changed=False, msg=RACK_NOT_FOUND),
+            mock.call(changed=True, msg=RACK_CREATED, ansible_facts=dict(rack=DEFAULT_RACK_TEMPLATE))]
+        mock_ansible_instance.exit_json.assert_has_calls(exit_json_calls)
+
+    @mock.patch('oneview_rack.resource_compare')
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_warning_rename_when_new_name_in_use(self, mock_rack_module, mock_from_json, mock_resource_compare):
+        mock_resource_compare.return_value = False
+
+        params_for_present = deepcopy(PARAMS_FOR_PRESENT)
+        params_for_present['data']['newName'] = "Rename the Rack"
+
+        rack_renamed = deepcopy(DEFAULT_RACK_TEMPLATE)
+        rack_renamed['name'] = "Rename the Rack"
+
+        fail_msg = "There is already a rack with the name 'Rename the Rack'"
+
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+
+        mock_ov_instance.racks.add.return_value = DEFAULT_RACK_TEMPLATE
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(params_for_present)
+        mock_rack_module.return_value = mock_ansible_instance
+        mock_ov_instance.racks.update.return_value = rack_renamed
+        mock_ov_instance.racks.update.side_effect = Exception(fail_msg)
+
+        RackModule().run()
+
+        mock_ov_instance.racks.update.assert_called_once_with(rack_renamed)
+
+        mock_ansible_instance.exit_json.assert_called_once_with(changed=False, msg=RACK_NEW_NAME_ALERADY_EXISTS)
+        mock_ansible_instance.fail_json.assert_called_once_with(msg=fail_msg)
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_fail_when_create_raises_exception(self, mock_rack_module, mock_from_json):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = []
+        mock_ov_instance.racks.add.side_effect = Exception(FAKE_MSG_ERROR)
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_PRESENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        self.assertRaises(Exception, RackModule().run())
+
+        mock_ansible_instance.fail_json.assert_called_once_with(
+            msg=FAKE_MSG_ERROR
+        )
+
+    @mock.patch('oneview_rack.resource_compare')
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_fail_when_update_raises_exception(self, mock_rack_module, mock_from_json, mock_resource_compare):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+        mock_ov_instance.racks.update.side_effect = Exception(FAKE_MSG_ERROR)
+
+        mock_resource_compare.return_value = False
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_PRESENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        self.assertRaises(Exception, RackModule().run())
+
+        mock_ansible_instance.fail_json.assert_called_once_with(
+            msg=FAKE_MSG_ERROR
+        )
+
+
+class RackAbsentStateSpec(unittest.TestCase):
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_remove_rack(self, mock_rack_module, mock_from_json):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_ABSENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        RackModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            msg=RACK_DELETED
+        )
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_do_nothing_when_rack_not_exist(self, mock_rack_module, mock_from_json):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = []
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_ABSENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        RackModule().run()
+
+        mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            msg=RACK_ALREADY_ABSENT
+        )
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch('oneview_rack.AnsibleModule')
+    def test_should_fail_when_delete_raises_exception(self, mock_rack_module, mock_from_json):
+        mock_ov_instance = mock.Mock()
+        mock_ov_instance.racks.get_by.return_value = [DEFAULT_RACK_TEMPLATE]
+        mock_ov_instance.racks.remove.side_effect = Exception(FAKE_MSG_ERROR)
+
+        mock_from_json.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock(PARAMS_FOR_ABSENT)
+        mock_rack_module.return_value = mock_ansible_instance
+
+        self.assertRaises(Exception, RackModule().run())
+
+        mock_ansible_instance.fail_json.assert_called_once_with(
+            msg=FAKE_MSG_ERROR
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
