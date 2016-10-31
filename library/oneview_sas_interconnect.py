eb647fbc7a98826e4e729a1d6031a4492441f5b0
Bruno Souza <bruno.souza@hpe.com>
2016-10-31 11:19:24 -0200

    Adds the sas_interconnect module

---
+++ library/oneview_sas_interconnect.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+from ansible.module_utils.basic import *
+try:
+    from hpOneView.oneview_client import OneViewClient
+
+    HAS_HPE_ONEVIEW = True
+except ImportError:
+    HAS_HPE_ONEVIEW = False
+
+DOCUMENTATION = '''
+---
+module: oneview_sas_interconnect
+short_description: Manage the OneView SAS Interconnect resources.
+description:
+    - Provides an interface to manage the SAS Interconnect. Can change the power state, UID light state, perform soft
+      and hard reset, and refresh the SAS Interconnect state.
+requirements:
+    - "python >= 2.7.9"
+    - "hpOneView >= 3.0.0"
+author: "Bruno Souza (@bsouza)"
+options:
+    config:
+      description:
+        - Path to a .json configuration file containing the OneView client configuration.
+          The configuration file is optional. If the file path is not provided, the configuration will be loaded from
+          environment variables.
+      required: false
+    state:
+        description:
+            - Indicates the desired state for the Switch.
+              'powered_on' turns the power on.
+              'powered_off' turns the power off.
+              'uid_on' turns the UID light on.
+              'uid_off' turns the UID light off.
+              'soft_reset' perform a soft reset.
+              'hard_reset' perform a hard reset.
+              'refreshed' perform a refresh.
+        choices: ['powered_on', 'powered_off', 'uid_on', 'uid_off', 'soft_reset', 'hard_reset', 'refreshed']
+    name:
+      description:
+        - The SAS Interconnect name.
+      required: True
+notes:
+    - "A sample configuration file for the config parameter can be found at:
+       https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
+    - "Check how to use environment variables for configuration at:
+       https://github.com/HewlettPackard/oneview-ansible#environment-variables"
+'''
+
+EXAMPLES = '''
+- name: Ensure that a SAS Interconnect is powered on
+  oneview_sas_interconnect:
+    config: "{{ config }}"
+    state: powered_on
+    name: "0000A66101, interconnect 1"
+
+- name: Refresh a SAS Interconnect
+  oneview_sas_interconnect:
+    config: "{{ config }}"
+    state: refreshed
+    name: "0000A66101, interconnect 1"
+
+- name: Perform a hard reset
+  oneview_sas_interconnect:
+    config: "{{ config }}"
+    state: hard_reset
+    name: "0000A66101, interconnect 1"
+'''
+
+HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
+
+
+class SasInterconnectModule(object):
+
+    states = dict(
+        refreshed=dict(),
+        powered_on=dict(operation='replace', path='/powerState', value='On'),
+        powered_off=dict(operation='replace', path='/powerState', value='Off'),
+        uid_on=dict(operation='replace', path='/uidState', value='On'),
+        uid_off=dict(operation='replace', path='/uidState', value='Off'),
+        soft_reset=dict(operation='replace', path='/softResetState', value='Reset'),
+        hard_reset=dict(operation='replace', path='/hardResetState', value='Reset')
+    )
+
+    argument_spec = dict(
+        config=dict(required=False, type='str'),
+        state=dict(
+            required=True,
+            choices=states.keys()
+        ),
+        name=dict(required=True, type='str')
+    )
+
+    actions = ['soft_reset', 'hard_reset']
+
+    def __init__(self):
+        self.module = AnsibleModule(
+            argument_spec=self.argument_spec,
+            supports_check_mode=False
+        )
+        if not HAS_HPE_ONEVIEW:
+            self.module.fail_json(msg=HPE_ONEVIEW_SDK_REQUIRED)
+
+        config = self.module.params['config']
+
+        if config:
+            oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+        else:
+            oneview_client = OneViewClient.from_environment_variables()
+
+        self.resource_client = oneview_client.sas_interconnects
+
+    def run(self):
+        try:
+            name = self.module.params.get('name')
+            state_name = self.module.params.get('state')
+
+            facts = {}
+            sas_interconnect = self.__get_by_name(name)
+
+            if state_name == 'refreshed':
+                facts['sas_interconnect'] = self.resource_client.refresh_state(
+                    id_or_uri=sas_interconnect['uri'],
+                    configuration=dict(refreshState="RefreshPending")
+                )
+                changed = True
+            else:
+                changed, facts['sas_interconnect'] = self.change_state(state_name, sas_interconnect)
+
+            self.module.exit_json(changed=changed, ansible_facts=facts)
+        except Exception as exception:
+            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
+
+    def __get_by_name(self, name):
+        sas_interconnects = self.resource_client.get_by('name', name)
+
+        if not sas_interconnects:
+            raise Exception("SAS Interconnect not found.")
+
+        return sas_interconnects[0]
+
+    def change_state(self, state_name, resource):
+        changed = False
+        state = self.states[state_name]
+        property_name = state['path'][1:]
+
+        if state_name in self.actions or resource[property_name] != state['value']:
+            resource = self.resource_client.patch(id_or_uri=resource["uri"], **state)
+            changed = True
+
+        return changed, resource
+
+
+def main():
+    SasInterconnectModule().run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_oneview_sas_interconnect.py
@@ -0,0 +1,293 @@
+###
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+import unittest
+import mock
+
+from hpOneView.oneview_client import OneViewClient
+from oneview_sas_interconnect import SasInterconnectModule
+from utils import create_ansible_mock
+from mock import patch
+import copy
+
+
+SAS_INTERCONNECT_NAME = "0000A66103, interconnect 4"
+SAS_INTERCONNECT_URI = '/rest/sas-interconnects/3518be0e-17c1-4189-8f81-83f3724f6155'
+
+
+def create_params(state):
+    return dict(
+        config='config.json',
+        state=state,
+        name=SAS_INTERCONNECT_NAME
+    )
+
+REFRESH_CONFIGURATION = dict(refreshState="RefreshPending")
+
+PARAMS_FOR_REFRESH = create_params('refreshed')
+PARAMS_FOR_UID_ON = create_params('uid_on')
+PARAMS_FOR_UID_OFF = create_params('uid_off')
+PARAMS_FOR_POWERED_ON = create_params('powered_on')
+PARAMS_FOR_POWERED_OFF = create_params('powered_off')
+PARAMS_FOR_SOFT_RESET = create_params('soft_reset')
+PARAMS_FOR_HARD_RESET = create_params('hard_reset')
+
+SAS_INTERCONNECT = dict(
+    name=SAS_INTERCONNECT_NAME,
+    uri=SAS_INTERCONNECT_URI
+)
+
+
+class SasInterconnectModuleSpec(unittest.TestCase):
+
+    def setUp(self):
+        patcher = patch.object(OneViewClient, 'from_json_file')
+        mock_from_json_file = patcher.start()
+
+        mock_ov_client = mock.Mock()
+        mock_from_json_file.return_value = mock_ov_client
+
+        self.resource = mock_ov_client.sas_interconnects
+        self.addCleanup(patcher.stop)
+
+        patcher_ansible = patch('oneview_sas_interconnect.AnsibleModule')
+        mock_ansible_module = patcher_ansible.start()
+
+        self.mock_ansible_instance = mock.Mock()
+        mock_ansible_module.return_value = self.mock_ansible_instance
+
+        self.addCleanup(patcher_ansible.stop)
+
+    def test_should_refresh_the_sas_interconnect(self):
+        self.resource.get_by.return_value = [SAS_INTERCONNECT]
+        self.resource.refresh_state.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_REFRESH)
+
+        SasInterconnectModule().run()
+
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+
+        self.resource.refresh_state.assert_called_once_with(
+            id_or_uri=SAS_INTERCONNECT_URI,
+            configuration=REFRESH_CONFIGURATION
+        )
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_turn_on_the_uid_when_uid_is_off(self):
+        sas_interconnect = dict(uidState='Off', **SAS_INTERCONNECT)
+
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_UID_ON)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['uid_on']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_do_nothing_when_uid_is_already_on(self):
+        sas_interconnect = dict(uidState='On', **SAS_INTERCONNECT)
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_UID_ON)
+
+        SasInterconnectModule().run()
+
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_not_called()
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(sas_interconnect=sas_interconnect)
+        )
+
+    def test_should_turn_off_the_uid_when_uid_is_on(self):
+        sas_interconnect = dict(uidState='On', **SAS_INTERCONNECT)
+
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_UID_OFF)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['uid_off']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_do_nothing_when_uid_is_already_off(self):
+        sas_interconnect = dict(uidState='Off', **SAS_INTERCONNECT)
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_UID_OFF)
+
+        SasInterconnectModule().run()
+
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_not_called()
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(sas_interconnect=sas_interconnect)
+        )
+
+    def test_should_turn_the_power_off_when_the_sas_interconnect_is_powered_on(self):
+        sas_interconnect = dict(powerState='On', **SAS_INTERCONNECT)
+
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_POWERED_OFF)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['powered_off']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_do_nothing_when_the_sas_interconnect_is_already_powered_off(self):
+        sas_interconnect = dict(powerState='Off', **SAS_INTERCONNECT)
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_POWERED_OFF)
+
+        SasInterconnectModule().run()
+
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_not_called()
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(sas_interconnect=sas_interconnect)
+        )
+
+    def test_should_turn_the_power_on_when_the_sas_interconnect_is_powered_off(self):
+        sas_interconnect = dict(powerState='Off', **SAS_INTERCONNECT)
+
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_POWERED_ON)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['powered_on']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_do_nothing_when_the_sas_interconnect_is_already_powered_on(self):
+        sas_interconnect = dict(powerState='On', **SAS_INTERCONNECT)
+        self.resource.get_by.return_value = [sas_interconnect]
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_POWERED_ON)
+
+        SasInterconnectModule().run()
+
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_not_called()
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=False,
+            ansible_facts=dict(sas_interconnect=sas_interconnect)
+        )
+
+    def test_should_perform_soft_reset(self):
+        self.resource.get_by.return_value = [SAS_INTERCONNECT]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_SOFT_RESET)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['soft_reset']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+    def test_should_perform_hard_reset(self):
+        self.resource.get_by.return_value = [SAS_INTERCONNECT]
+        self.resource.patch.return_value = SAS_INTERCONNECT
+        self.mock_ansible_instance.params = copy.deepcopy(PARAMS_FOR_HARD_RESET)
+
+        SasInterconnectModule().run()
+
+        state = SasInterconnectModule.states['hard_reset']
+        self.resource.get_by.assert_called_once_with('name', SAS_INTERCONNECT_NAME)
+        self.resource.patch.assert_called_once_with(id_or_uri=SAS_INTERCONNECT_URI, **state)
+
+        self.mock_ansible_instance.exit_json.assert_called_once_with(
+            changed=True,
+            ansible_facts=dict(sas_interconnect=SAS_INTERCONNECT)
+        )
+
+
+class SasInterconnectClientConfigurationSpec(unittest.TestCase):
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch.object(OneViewClient, 'from_environment_variables')
+    @mock.patch('oneview_sas_interconnect.AnsibleModule')
+    def test_should_load_config_from_file(self, mock_ansible_module, mock_ov_client_from_env_vars,
+                                          mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+        mock_ov_client_from_json_file.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock({'config': 'config.json'})
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        SasInterconnectModule()
+
+        mock_ov_client_from_json_file.assert_called_once_with('config.json')
+        mock_ov_client_from_env_vars.not_been_called()
+
+    @mock.patch.object(OneViewClient, 'from_json_file')
+    @mock.patch.object(OneViewClient, 'from_environment_variables')
+    @mock.patch('oneview_sas_interconnect.AnsibleModule')
+    def test_should_load_config_from_environment(self, mock_ansible_module, mock_ov_client_from_env_vars,
+                                                 mock_ov_client_from_json_file):
+        mock_ov_instance = mock.Mock()
+
+        mock_ov_client_from_env_vars.return_value = mock_ov_instance
+        mock_ansible_instance = create_ansible_mock({'config': None})
+        mock_ansible_module.return_value = mock_ansible_instance
+
+        SasInterconnectModule()
+
+        mock_ov_client_from_env_vars.assert_called_once()
+        mock_ov_client_from_json_file.not_been_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
