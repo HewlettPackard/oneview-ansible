23fa173f79049cd76d3df280dcfe9f0a54ff9672
Gustavo Augusto Hennig <gustavoh@dbserver.com.br>
2017-01-05 17:27:12 -0200

    Implements "validate_etag" option; Unit tests refactoring.

--- library/oneview_datacenter.py
+++ library/oneview_datacenter.py
@@ -52,6 +52,12 @@ options:
         description:
             - List with Data Center properties and its associated states.
         required: true
+    validate_etag:
+        description:
+            - When the ETag Validation is enabled, the request will be conditionally processed only if the current ETag
+              for the resource matches the ETag provided in the data.
+        default: true
+        choices: ['true', 'false']
 notes:
     - "A sample configuration file for the config parameter can be found at:
        https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
@@ -136,7 +142,11 @@ class DatacenterModule(object):
             required=True,
             choices=['present', 'absent']
         ),
-        data=dict(required=True, type='dict')
+        data=dict(required=True, type='dict'),
+        validate_etag=dict(
+            required=False,
+            type='bool',
+            default=True)
     )
 
     def __init__(self):
@@ -151,6 +161,9 @@ class DatacenterModule(object):
 
     def run(self):
         try:
+            if not self.module.params.get('validate_etag'):
+                self.oneview_client.connection.disable_etag_validation()
+
             state = self.module.params['state']
             data = self.module.params['data']
             changed, msg, ansible_facts = False, '', {}
