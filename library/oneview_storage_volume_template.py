1ac59dba8e1c5bc24d36a2c55aba5431d28b50a6
Abilio Parada <abilio.parada@hpe.com>
2017-01-10 18:50:41 -0200

    Implements 'validate-etag' option for Storage Volume Template

--- library/oneview_storage_volume_template.py
+++ library/oneview_storage_volume_template.py
@@ -52,6 +52,12 @@ options:
         description:
             - List with Storage Volume Template properties and its associated states.
         required: true
+    validate_etag:
+        description:
+            - When the ETag Validation is enabled, the request will be conditionally processed only if the current ETag
+              for the resource matches the ETag provided in the data.
+        default: true
+        choices: ['true', 'false']
 notes:
     - "A sample configuration file for the config parameter can be found at:
        https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
@@ -112,11 +118,18 @@ class StorageVolumeTemplateModule(object):
             required=True,
             choices=['present', 'absent']
         ),
-        data=dict(required=True, type='dict')
+        data=dict(required=True, type='dict'),
+        validate_etag=dict(
+            required=False,
+            type='bool',
+            default=True)
     )
 
     def __init__(self):
-        self.module = AnsibleModule(argument_spec=self.argument_spec, supports_check_mode=False)
+        self.module = AnsibleModule(
+            argument_spec=self.argument_spec,
+            supports_check_mode=False
+        )
         if not HAS_HPE_ONEVIEW:
             self.module.fail_json(msg=HPE_ONEVIEW_SDK_REQUIRED)
 
@@ -130,6 +143,9 @@ class StorageVolumeTemplateModule(object):
             state = self.module.params['state']
             data = self.module.params['data']
             changed, msg, ansible_facts = False, '', {}
+
+            if not self.module.params.get('validate_etag'):
+                self.oneview_client.connection.disable_etag_validation()
 
             if not data.get('name'):
                 raise Exception(STORAGE_VOLUME_TEMPLATE_MANDATORY_FIELD_MISSING)
