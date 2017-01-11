273dbf845191ded451945e7ae300ed94df30df94
Gustavo Augusto Hennig <gustavoh@dbserver.com.br>
2017-01-11 13:39:50 -0200

    Implements 'validate-etag' option for Server Hardware Type

--- library/oneview_server_hardware_type.py
+++ library/oneview_server_hardware_type.py
@@ -51,6 +51,12 @@ options:
         description:
             - List with Server Hardware Type properties and its associated states.
         required: true
+    validate_etag:
+        description:
+            - When the ETag Validation is enabled, the request will be conditionally processed only if the current ETag
+              for the resource matches the ETag provided in the data.
+        default: true
+        choices: ['true', 'false']
 notes:
     - "A sample configuration file for the config parameter can be found at:
        https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
@@ -108,7 +114,11 @@ class ServerHardwareTypeModule(object):
             required=True,
             choices=['present', 'absent']
         ),
-        data=dict(required=True, type='dict')
+        data=dict(required=True, type='dict'),
+        validate_etag=dict(
+            required=False,
+            type='bool',
+            default=True)
     )
 
     def __init__(self):
@@ -123,6 +133,9 @@ class ServerHardwareTypeModule(object):
 
     def run(self):
         try:
+            if not self.module.params.get('validate_etag'):
+                self.oneview_client.connection.disable_etag_validation()
+
             state = self.module.params['state']
             data = self.module.params['data']
             changed, msg, ansible_facts = False, '', {}
--- test/test_oneview_server_hardware_type.py
+++ test/test_oneview_server_hardware_type.py
@@ -14,15 +14,15 @@
 # limitations under the License.
 ###
 import unittest
-import mock
 import yaml
 
-from hpOneView.oneview_client import OneViewClient
-from oneview_server_hardware_type import ServerHardwareTypeModule, SERVER_HARDWARE_TYPE_DELETED, \
-    SERVER_HARDWARE_TYPE_ALREADY_ABSENT, SERVER_HARDWARE_TYPE_ALREADY_UPDATED, SERVER_HARDWARE_TYPE_UPDATED, \
-    SERVER_HARDWARE_TYPE_NOT_FOUND
-from test.utils import create_ansible_mock
-from test.utils import create_ansible_mock_yaml
+from oneview_server_hardware_type import (ServerHardwareTypeModule,
+                                          SERVER_HARDWARE_TYPE_DELETED,
+                                          SERVER_HARDWARE_TYPE_ALREADY_ABSENT,
+                                          SERVER_HARDWARE_TYPE_ALREADY_UPDATED,
+                                          SERVER_HARDWARE_TYPE_UPDATED,
+                                          SERVER_HARDWARE_TYPE_NOT_FOUND)
+from test.utils import ModuleContructorTestCase, ValidateEtagTestCase
 
 FAKE_MSG_ERROR = 'Fake message error'
 
@@ -54,170 +54,105 @@ DICT_DEFAULT_SERVER_HARDWARE_TYPE = yaml.load(YAML_SERVER_HARDWARE_TYPE)["data"]
 DICT_DEFAULT_SERVER_HARDWARE_TYPE_CHANGED = yaml.load(YAML_SERVER_HARDWARE_TYPE_CHANGE)["data"]
 
 
-class ServerHardwareTypeClientConfigurationSpec(unittest.TestCase):
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch.object(OneViewClient, 'from_environment_variables')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_load_config_from_file(self, mock_ansible_module, mock_ov_client_from_env_vars,
-                                          mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock({'config': 'config.json'})
-        mock_ansible_module.return_value = mock_ansible_instance
+class ServerHardwareTypeSpec(unittest.TestCase, ModuleContructorTestCase, ValidateEtagTestCase):
+    """
+    ModuleContructorTestCase has common tests for class constructor and main function
+    ValidateEtagTestCase has common tests for the validate_etag attribute, also provides the mocks used in this test
+    case.
+    """
 
-        ServerHardwareTypeModule()
+    def setUp(self):
+        self.configure_mocks(self, ServerHardwareTypeModule)
+        self.resource = self.mock_ov_client.server_hardware_types
 
-        mock_ov_client_from_json_file.assert_called_once_with('config.json')
-        mock_ov_client_from_env_vars.not_been_called()
-
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch.object(OneViewClient, 'from_environment_variables')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_load_config_from_environment(self, mock_ansible_module, mock_ov_client_from_env_vars,
-                                                 mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-
-        mock_ov_client_from_env_vars.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock({'config': None})
-        mock_ansible_module.return_value = mock_ansible_instance
-
-        ServerHardwareTypeModule()
-
-        mock_ov_client_from_env_vars.assert_called_once()
-        mock_ov_client_from_json_file.not_been_called()
-
-
-class ServerHardwareTypePresentStateSpec(unittest.TestCase):
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_update_the_server_hardware_type(self, mock_ansible_module, mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-
+    def test_should_update_the_server_hardware_type(self):
         srv_hw_type = DICT_DEFAULT_SERVER_HARDWARE_TYPE.copy()
         srv_hw_type['uri'] = '/rest/id'
 
-        mock_ov_instance.server_hardware_types.get_by.return_value = [srv_hw_type]
-        mock_ov_instance.server_hardware_types.update.return_value = {"name": "name"}
+        self.resource.get_by.return_value = [srv_hw_type]
+        self.resource.update.return_value = {"name": "name"}
 
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE_CHANGE)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE_CHANGE)
 
         ServerHardwareTypeModule().run()
 
-        mock_ansible_instance.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=SERVER_HARDWARE_TYPE_UPDATED,
             ansible_facts=dict(server_hardware_type={"name": "name"})
         )
 
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_not_update_when_data_is_equals(self, mock_ansible_module, mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
+    def test_should_not_update_when_data_is_equals(self):
+        self.resource.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
 
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE)
 
         ServerHardwareTypeModule().run()
 
-        mock_ansible_instance.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=False,
             msg=SERVER_HARDWARE_TYPE_ALREADY_UPDATED,
             ansible_facts=dict(server_hardware_type=DICT_DEFAULT_SERVER_HARDWARE_TYPE)
         )
 
+    def test_should_remove_server_hardware_type(self):
+        self.resource.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
 
-class ServerHardwareTypeAbsentStateSpec(unittest.TestCase):
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_remove_server_hardware_type(self, mock_ansible_module, mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
-
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE_ABSENT)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE_ABSENT)
 
         ServerHardwareTypeModule().run()
 
-        mock_ansible_instance.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             ansible_facts={},
             changed=True,
             msg=SERVER_HARDWARE_TYPE_DELETED
         )
 
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_do_nothing_when_server_hardware_type_not_exist(self, mock_ansible_module,
-                                                                   mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = []
+    def test_should_do_nothing_when_server_hardware_type_not_exist(self):
+        self.resource.get_by.return_value = []
 
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE_ABSENT)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE_ABSENT)
 
         ServerHardwareTypeModule().run()
 
-        mock_ansible_instance.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             ansible_facts={},
             changed=False,
             msg=SERVER_HARDWARE_TYPE_ALREADY_ABSENT
         )
 
+    def test_should_fail_when_update_raises_exception(self):
+        self.resource.get_by.return_value = [{'uri': 'test'}]
+        self.resource.update.side_effect = Exception(FAKE_MSG_ERROR)
 
-class ServerHardwareTypeErrorHandlingSpec(unittest.TestCase):
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_fail_when_update_raises_exception(self, mock_ansible_module, mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = [{'uri': 'test'}]
-        mock_ov_instance.server_hardware_types.update.side_effect = Exception(FAKE_MSG_ERROR)
-
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE)
 
         self.assertRaises(Exception, ServerHardwareTypeModule().run())
 
-        mock_ansible_instance.fail_json.assert_called_once_with(
+        self.mock_ansible_module.fail_json.assert_called_once_with(
             msg=FAKE_MSG_ERROR
         )
 
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_fail_when_server_hardware_type_was_not_found(self, mock_ansible_module,
-                                                                 mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = []
+    def test_should_fail_when_server_hardware_type_was_not_found(self):
+        self.resource.get_by.return_value = []
 
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE)
 
         self.assertRaises(Exception, ServerHardwareTypeModule().run())
 
-        mock_ansible_instance.fail_json.assert_called_once_with(
+        self.mock_ansible_module.fail_json.assert_called_once_with(
             msg=SERVER_HARDWARE_TYPE_NOT_FOUND
         )
 
-    @mock.patch.object(OneViewClient, 'from_json_file')
-    @mock.patch('oneview_server_hardware_type.AnsibleModule')
-    def test_should_fail_when_delete_raises_exception(self, mock_ansible_module, mock_ov_client_from_json_file):
-        mock_ov_instance = mock.Mock()
-        mock_ov_instance.server_hardware_types.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
-        mock_ov_instance.server_hardware_types.delete.side_effect = Exception(FAKE_MSG_ERROR)
+    def test_should_fail_when_delete_raises_exception(self):
+        self.resource.get_by.return_value = [DICT_DEFAULT_SERVER_HARDWARE_TYPE]
+        self.resource.delete.side_effect = Exception(FAKE_MSG_ERROR)
 
-        mock_ov_client_from_json_file.return_value = mock_ov_instance
-        mock_ansible_instance = create_ansible_mock_yaml(YAML_SERVER_HARDWARE_TYPE_ABSENT)
-        mock_ansible_module.return_value = mock_ansible_instance
+        self.mock_ansible_module.params = yaml.load(YAML_SERVER_HARDWARE_TYPE_ABSENT)
 
         self.assertRaises(Exception, ServerHardwareTypeModule().run())
 
-        mock_ansible_instance.fail_json.assert_called_once_with(
+        self.mock_ansible_module.fail_json.assert_called_once_with(
             msg=FAKE_MSG_ERROR
         )
 
