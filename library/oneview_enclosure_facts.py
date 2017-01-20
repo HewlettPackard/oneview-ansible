5a2d47e9e749f8ea9e097a9cae73c7fbe46041fd
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-01-20 20:23:36 +0000

    Error handling improvements - Enclosures

--- library/oneview_enclosure_facts.py
+++ library/oneview_enclosure_facts.py
@@ -20,6 +20,7 @@ from ansible.module_utils.basic import *
 try:
     from hpOneView.oneview_client import OneViewClient
     from hpOneView.common import transform_list_to_dict
+    from hpOneView.exceptions import HPOneViewException
 
     HAS_HPE_ONEVIEW = True
 except ImportError:
@@ -190,7 +191,7 @@ class EnclosureFactsModule(object):
             self.module.exit_json(changed=False,
                                   ansible_facts=ansible_facts)
 
-        except Exception as exception:
+        except HPOneViewException as exception:
             self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
 
     def __gather_optional_facts(self, options, enclosure):
