68e014df7d063c408c30558bfbb84addf082d9f8
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-01-20 19:23:06 +0000

    Error handling improvements - Connection Template Modules

--- library/oneview_connection_template_facts.py
+++ library/oneview_connection_template_facts.py
@@ -19,6 +19,7 @@ from ansible.module_utils.basic import *
 
 try:
     from hpOneView.oneview_client import OneViewClient
+    from hpOneView.exceptions import HPOneViewException
 
     HAS_HPE_ONEVIEW = True
 except ImportError:
@@ -161,7 +162,7 @@ class ConnectionTemplateFactsModule(object):
             self.module.exit_json(changed=False,
                                   ansible_facts=ansible_facts)
 
-        except Exception as exception:
+        except HPOneViewException as exception:
             self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
 
 
