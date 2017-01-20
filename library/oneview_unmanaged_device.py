e831a2fa8c090306ccc77a54f3f19e5610e41f4e
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-01-20 20:45:49 +0000

    Error handling improvements - Unmanaged Device

--- library/oneview_unmanaged_device.py
+++ library/oneview_unmanaged_device.py
@@ -19,6 +19,7 @@ from ansible.module_utils.basic import *
 try:
     from hpOneView.oneview_client import OneViewClient
     from hpOneView.common import resource_compare
+    from hpOneView.exceptions import HPOneViewException
 
     HAS_HPE_ONEVIEW = True
 except ImportError:
@@ -172,7 +173,7 @@ class UnmanagedDeviceModule(object):
                 changed, msg = self.__absent(data)
 
             self.module.exit_json(changed=changed, msg=msg, **facts)
-        except Exception as exception:
+        except HPOneViewException as exception:
             self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
 
     def __present(self, data):
--- library/oneview_unmanaged_device_facts.py
+++ library/oneview_unmanaged_device_facts.py
@@ -19,6 +19,7 @@
 from ansible.module_utils.basic import *
 try:
     from hpOneView.oneview_client import OneViewClient
+    from hpOneView.exceptions import HPOneViewException
 
     HAS_HPE_ONEVIEW = True
 except ImportError:
@@ -153,7 +154,7 @@ class UnmanagedDeviceFactsModule(object):
 
             facts["unmanaged_devices"] = unmanaged_devices
             self.module.exit_json(ansible_facts=facts)
-        except Exception as exception:
+        except HPOneViewException as exception:
             self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
 
     def __get_environmental_configuration(self, unmanaged_devices):
