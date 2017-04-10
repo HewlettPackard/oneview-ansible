387fd07e40b3d4a86d39310884c95ccc11e3fdba
Gustavo Augusto Hennig <gustavoh@dbserver.com.br>
2017-04-10 15:46:58 -0300

    Adjusting OS Deployment Server for Ansible core

--- library/oneview_os_deployment_server.py
+++ library/oneview_os_deployment_server.py
@@ -15,17 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###
-from ansible.module_utils.basic import *
 
-try:
-    from hpOneView.oneview_client import OneViewClient
-    from hpOneView.extras.comparators import resource_compare
-    from hpOneView.exceptions import HPOneViewException
-    from hpOneView.exceptions import HPOneViewResourceNotFound
-
-    HAS_HPE_ONEVIEW = True
-except ImportError:
-    HAS_HPE_ONEVIEW = False
+ANSIBLE_METADATA = {'status': ['stableinterface'],
+                    'supported_by': 'curated',
+                    'metadata_version': '1.0'}
 
 DOCUMENTATION = '''
 ---
@@ -33,41 +26,30 @@ module: oneview_os_deployment_server
 short_description: Manage OneView Deployment Server resources.
 description:
     - Provides an interface to manage Deployment Server resources. Can create, update, or delete.
+version_added: "2.3"
 requirements:
     - "python >= 2.7.9"
     - "hpOneView >= 3.1.1"
 author: "Camila Balestrin (@balestrinc)"
 options:
-    config:
-      description:
-        - Path to a .json configuration file containing the OneView client configuration.
-          The configuration file is optional. If the file path is not provided, the configuration will be loaded from
-          environment variables.
-      required: false
     state:
         description:
             - Indicates the desired state for the Deployment Server resource.
-              'present' will ensure data properties are compliant with OneView.
-              'absent' will remove the resource from OneView, if it exists.
+              C(present) will ensure data properties are compliant with OneView.
+              C(absent) will remove the resource from OneView, if it exists.
         choices: ['present', 'absent']
     data:
         description:
             - List with Deployment Server properties.
         required: true
-    validate_etag:
-        description:
-            - When the ETag Validation is enabled, the request will be conditionally processed only if the current ETag
-              for the resource matches the ETag provided in the data.
-        default: true
-        choices: ['true', 'false']
 notes:
-    - "A sample configuration file for the config parameter can be found at:
-       https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
-    - "Check how to use environment variables for configuration at:
-       https://github.com/HewlettPackard/oneview-ansible#environment-variables"
-    - "For the following data, you can provide either a name  or a URI: mgmtNetworkName or mgmtNetworkUri, and
-      applianceName or applianceUri"
+    - "For the following data, you can provide either a name or a URI: C(mgmtNetworkName) or C(mgmtNetworkUri), and
+      C(applianceName) or C(applianceUri)"
     - This resource is only available on HPE Synergy
+
+extends_documentation_fragment:
+    - oneview
+    - oneview.validateetag
 '''
 
 EXAMPLES = '''
@@ -80,7 +62,6 @@ EXAMPLES = '''
       description: "OS Deployment Server"
       mgmtNetworkUri: "/rest/ethernet-networks/1b96d2b3-bc12-4757-ac72-e4cd0ef20535"
       applianceUri: "/rest/deployment-servers/image-streamer-appliances/aca554e2-09c2-4b14-891d-e51c0058efab"
-
 - debug: var=os_deployment_server
 
 - name: Ensure that the Deployment Server is present with name 'Renamed Deployment Server'
@@ -90,9 +71,7 @@ EXAMPLES = '''
     data:
       name: 'Test Deployment Server'
       newName: 'Renamed Deployment Server'
-
 - debug: var=os_deployment_server
-
 
 - name: Ensure that the Deployment Server is absent
   oneview_os_deployment_server:
@@ -109,63 +88,39 @@ os_deployment_server:
     type: complex
 '''
 
+from ansible.module_utils.basic import AnsibleModule
+from module_utils.oneview import OneViewModuleBase, HPOneViewResourceNotFound, ResourceComparator
 
-class OsDeploymentServerModule(object):
-    DEPLOYMENT_SERVER_CREATED = 'Deployment Server created successfully.'
-    DEPLOYMENT_SERVER_UPDATED = 'Deployment Server updated successfully.'
-    DEPLOYMENT_SERVER_DELETED = 'Deployment Server deleted successfully.'
-    DEPLOYMENT_SERVER_ALREADY_EXIST = 'Deployment Server already exists.'
-    DEPLOYMENT_SERVER_ALREADY_ABSENT = 'Deployment Server is already absent.'
-    HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
-    NETWORK_NOT_FOUND = 'Network "{}" not found.'
-    APPLIANCE_NOT_FOUND = 'Appliance "{}" not found.'
+
+class OsDeploymentServerModule(OneViewModuleBase):
+    MSG_CREATED = 'Deployment Server created successfully.'
+    MSG_UPDATED = 'Deployment Server updated successfully.'
+    MSG_DELETED = 'Deployment Server deleted successfully.'
+    MSG_ALREADY_EXIST = 'Deployment Server already exists.'
+    MSG_ALREADY_ABSENT = 'Deployment Server is already absent.'
+    MSG_NETWORK_NOT_FOUND = 'Network "{}" not found.'
+    MSG_APPLIANCE_NOT_FOUND = 'Appliance "{}" not found.'
 
     argument_spec = dict(
-        config=dict(required=False, type='str'),
         state=dict(
             required=True,
             choices=['present', 'absent']
         ),
-        data=dict(required=True, type='dict'),
-        validate_etag=dict(
-            required=False,
-            type='bool',
-            default=True)
+        data=dict(required=True, type='dict')
     )
 
     def __init__(self):
-        self.module = AnsibleModule(argument_spec=self.argument_spec, supports_check_mode=False)
-        if not HAS_HPE_ONEVIEW:
-            self.module.fail_json(msg=self.HPE_ONEVIEW_SDK_REQUIRED)
+        super(OsDeploymentServerModule, self).__init__(additional_arg_spec=self.argument_spec,
+                                                       validate_etag_support=True)
+        self.resource_client = self.oneview_client.os_deployment_servers
 
-        if not self.module.params['config']:
-            self.oneview_client = OneViewClient.from_environment_variables()
-        else:
-            self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+    def execute_module(self):
+        resource = self.get_by_name(self.data['name'])
 
-    def run(self):
-
-        try:
-            state = self.module.params['state']
-            data = self.module.params['data']
-            changed, msg, ansible_facts = False, '', {}
-
-            if not self.module.params.get('validate_etag'):
-                self.oneview_client.connection.disable_etag_validation()
-
-            resource = self.oneview_client.os_deployment_servers.get_by_name(data['name'])
-
-            if state == 'present':
-                changed, msg, ansible_facts = self.__present(resource, data)
-            elif state == 'absent':
-                changed, msg, ansible_facts = self.__absent(resource)
-
-            self.module.exit_json(changed=changed,
-                                  msg=msg,
-                                  ansible_facts=ansible_facts)
-
-        except HPOneViewException as exception:
-            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
+        if self.state == 'present':
+            return self.__present(resource, self.data)
+        elif self.state == 'absent':
+            return self.resource_absent(resource)
 
     def __present(self, resource, data):
 
@@ -179,7 +134,7 @@ class OsDeploymentServerModule(object):
         if not resource:
             resource = self.oneview_client.os_deployment_servers.add(data)
             changed = True
-            msg = self.DEPLOYMENT_SERVER_CREATED
+            msg = self.MSG_CREATED
         else:
             appliance_uri = data.pop('applianceUri', '')
             if appliance_uri:
@@ -188,21 +143,16 @@ class OsDeploymentServerModule(object):
             merged_data = resource.copy()
             merged_data.update(data)
 
-            if not resource_compare(resource, merged_data):
+            if not ResourceComparator.compare(resource, merged_data):
                 resource = self.oneview_client.os_deployment_servers.update(merged_data)
                 changed = True
-                msg = self.DEPLOYMENT_SERVER_UPDATED
+                msg = self.MSG_UPDATED
             else:
-                msg = self.DEPLOYMENT_SERVER_ALREADY_EXIST
+                msg = self.MSG_ALREADY_EXIST
 
-        return changed, msg, dict(os_deployment_server=resource)
-
-    def __absent(self, resource):
-        if resource:
-            self.oneview_client.os_deployment_servers.delete(resource)
-            return True, self.DEPLOYMENT_SERVER_DELETED, {}
-
-        return False, self.DEPLOYMENT_SERVER_ALREADY_ABSENT, {}
+        return dict(changed=changed,
+                    msg=msg,
+                    ansible_facts=dict(os_deployment_server=resource))
 
     def __replace_names_by_uris(self, data):
         mgmt_network_name = data.pop("mgmtNetworkName", "")
@@ -224,7 +174,7 @@ class OsDeploymentServerModule(object):
 
         fcoe_networks = self.oneview_client.fcoe_networks.get_by('name', name)
         if not fcoe_networks:
-            raise HPOneViewResourceNotFound(self.NETWORK_NOT_FOUND.format(name))
+            raise HPOneViewResourceNotFound(self.MSG_NETWORK_NOT_FOUND.format(name))
 
         return fcoe_networks[0]['uri']
 
@@ -232,7 +182,7 @@ class OsDeploymentServerModule(object):
         appliance = self.oneview_client.os_deployment_servers.get_appliance_by_name(name)
 
         if not appliance:
-            raise HPOneViewResourceNotFound(self.APPLIANCE_NOT_FOUND.format(name))
+            raise HPOneViewResourceNotFound(self.MSG_APPLIANCE_NOT_FOUND.format(name))
 
         return appliance['uri']
 
