ac92022bf62e447ed187f124ea5498fe8433b2a6
Abilio Parada <abilio.parada@hpe.com>
2017-04-11 14:49:46 -0300

    Update SAS Logical Interconnect

--- library/oneview_sas_logical_interconnect.py
+++ library/oneview_sas_logical_interconnect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 ###
-# Copyright (2016) Hewlett Packard Enterprise Development LP
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -16,58 +16,47 @@
 # limitations under the License.
 ###
 
-from ansible.module_utils.basic import *
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'curated'}
 
-try:
-    from hpOneView.oneview_client import OneViewClient
-    from hpOneView.exceptions import HPOneViewException
-    from hpOneView.exceptions import HPOneViewResourceNotFound
-    from hpOneView.exceptions import HPOneViewValueError
-
-    HAS_HPE_ONEVIEW = True
-except ImportError:
-    HAS_HPE_ONEVIEW = False
-
-DOCUMENTATION = """
+DOCUMENTATION = '''
 ---
 module: oneview_sas_logical_interconnect
 short_description: Manage OneView SAS Logical Interconnect resources.
 description:
-    - Provides an interface to manage SAS Logical Interconnect resources.
+    - "Provides an interface to manage SAS Logical Interconnect resources."
+version_added: "2.3"
 requirements:
     - "python >= 2.7.9"
     - "hpOneView >= 3.0.0"
 author: "Gustavo Hennig (@GustavoHennig)"
 options:
-    config:
-      description:
-        - Path to a .json configuration file containing the OneView client configuration.
-          The configuration file is optional. If the file path is not provided, the configuration will be loaded from
-          environment variables.
-      required: false
     state:
         description:
             - Indicates the desired state for the SAS Logical Interconnect resources.
-              'compliant' brings the list of SAS Logical Interconnect back to a consistent state.
-              'configuration_updated' asynchronously applies or re-applies the SAS Logical Interconnect configuration
+              C(compliant) brings the list of SAS Logical Interconnect back to a consistent state.
+              C(configuration_updated) asynchronously applies or re-applies the SAS Logical Interconnect configuration
               to all managed interconnects.
-              'firmware_updated' installs firmware to a SAS Logical Interconnect.
-              'drive_enclosure_replaced' replacement operation of a drive enclosure.
+              C(firmware_updated) installs firmware to a SAS Logical Interconnect.
+              C(drive_enclosure_replaced) replacement operation of a drive enclosure.
               * All of them are non-idempotent.
         choices: ['compliant', 'drive_enclosure_replaced', 'configuration_updated', 'firmware_updated']
+        required: true
     data:
       description:
-        - List with the options.
+        - List with SAS Logical Interconnect properties and its associated states.
       required: true
-notes:
-    - "A sample configuration file for the config parameter can be found at:
-       https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
-    - "Check how to use environment variables for configuration at:
-       https://github.com/HewlettPackard/oneview-ansible#environment-variables"
-    - This resource is only available on HPE Synergy
-"""
 
-EXAMPLES = """
+notes:
+    - This resource is only available on HPE Synergy
+
+extends_documentation_fragment:
+    - oneview
+'''
+
+
+EXAMPLES = '''
 - name: Update the configuration on the SAS Logical Interconnect
   oneview_sas_logical_interconnect:
     config: "{{ config }}"
@@ -117,9 +106,9 @@ EXAMPLES = """
         '/rest/sas-logical-interconnects/16b2990f-944a-449a-a78f-004d8b4e6824',
         '/rest/sas-logical-interconnects/c800b2e4-92bb-44fa-8a46-f71d40737fa5']
   delegate_to: localhost
-"""
+'''
 
-RETURN = """
+RETURN = '''
 sas_logical_interconnect:
     description: Has the OneView facts about the SAS Logical Interconnect.
     returned: On states 'drive_enclosure_replaced', 'configuration_updated', but can be null.
@@ -129,119 +118,95 @@ li_firmware:
     description: Has the OneView facts about the updated Firmware.
     returned: On 'firmware_updated' state, but can be null.
     type: complex
-"""
+'''
 
-SAS_LOGICAL_INTERCONNECT_CONSISTENT = 'SAS Logical Interconnect returned to a consistent state.'
-SAS_LOGICAL_INTERCONNECT_CONFIGURATION_UPDATED = 'Configuration on the SAS Logical Interconnect updated successfully.'
-SAS_LOGICAL_INTERCONNECT_FIRMWARE_UPDATED = 'Firmware updated successfully.'
-SAS_LOGICAL_INTERCONNECT_DRIVE_ENCLOSURE_REPLACED = 'Drive enclosure replaced successfully.'
-SAS_LOGICAL_INTERCONNECT_NOT_FOUND = 'SAS Logical Interconnect not found.'
-SAS_LOGICAL_INTERCONNECT_NO_OPTIONS_PROVIDED = 'No options provided.'
-HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
+from ansible.module_utils.basic import AnsibleModule
+from module_utils.oneview import OneViewModuleBase, HPOneViewResourceNotFound, HPOneViewValueError
 
 
-class SasLogicalInterconnectModule(object):
-    argument_spec = dict(
-        config=dict(required=False, type='str'),
-        state=dict(
-            required=True,
-            choices=['compliant', 'drive_enclosure_replaced', 'configuration_updated', 'firmware_updated']
-        ),
-        data=dict(required=True, type='dict')
-    )
+class SasLogicalInterconnectModule(OneViewModuleBase):
+    MSG_CONSISTENT = 'SAS Logical Interconnect returned to a consistent state.'
+    MSG_CONFIGURATION_UPDATED = 'Configuration on the SAS Logical Interconnect updated successfully.'
+    MSG_FIRMWARE_UPDATED = 'Firmware updated successfully.'
+    MSG_DRIVE_ENCLOSURE_REPLACED = 'Drive enclosure replaced successfully.'
+    MSG_NOT_FOUND = 'SAS Logical Interconnect not found.'
+    MSG_NO_OPTIONS_PROVIDED = 'No options provided.'
 
     def __init__(self):
-        self.module = AnsibleModule(argument_spec=self.argument_spec, supports_check_mode=False)
-        if not HAS_HPE_ONEVIEW:
-            self.module.fail_json(msg=HPE_ONEVIEW_SDK_REQUIRED)
+        additional_arg_spec = dict(
+            data=dict(required=True, type='dict'),
+            state=dict(
+                required=True,
+                choices=['compliant', 'drive_enclosure_replaced', 'configuration_updated', 'firmware_updated']
+            )
+        )
 
-        if not self.module.params['config']:
-            self.oneview_client = OneViewClient.from_environment_variables()
+        super(SasLogicalInterconnectModule, self).__init__(additional_arg_spec=additional_arg_spec)
+
+        self.resource_client = self.oneview_client.sas_logical_interconnects
+
+    def execute_module(self):
+        if self.state == 'compliant':
+            changed, msg, ansible_facts = self.__compliance()
+
         else:
-            self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+            resource = self.get_by_name(self.data['name'])
 
-    def run(self):
-        state = self.module.params['state']
-        data = self.module.params['data']
+            if not resource:
+                raise HPOneViewResourceNotFound(self.MSG_NOT_FOUND)
 
-        try:
-            changed, msg, ansible_facts = False, '', {}
+            if self.state == 'configuration_updated':
+                changed, msg, ansible_facts = self.__update_configuration(resource['uri'])
+            elif self.state == 'firmware_updated':
+                changed, msg, ansible_facts = self.__update_firmware(resource['uri'])
+            elif self.state == 'drive_enclosure_replaced':
+                changed, msg, ansible_facts = self.__replace_drive_enclosure(resource['uri'])
 
-            if state == 'compliant':
-                changed, msg, ansible_facts = self.__compliance(data)
-            else:
+        return dict(changed=changed,
+                    msg=msg,
+                    ansible_facts=ansible_facts)
 
-                resource = self.__get_log_interconnect_by_name(data['name'])
-
-                if not resource:
-                    raise HPOneViewResourceNotFound(SAS_LOGICAL_INTERCONNECT_NOT_FOUND)
-
-                uri = resource['uri']
-
-                if state == 'configuration_updated':
-                    changed, msg, ansible_facts = self.__update_configuration(uri)
-                elif state == 'firmware_updated':
-                    changed, msg, ansible_facts = self.__update_firmware(uri, data)
-                elif state == 'drive_enclosure_replaced':
-                    changed, msg, ansible_facts = self.__replace_drive_enclosure(uri, data)
-
-            self.module.exit_json(changed=changed,
-                                  msg=msg,
-                                  ansible_facts=ansible_facts)
-
-        except HPOneViewException as exception:
-            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
-
-    def __compliance(self, data):
-
-        uris = data.get('logicalInterconnectUris')
-
+    def __compliance(self):
+        uris = self.data.get('logicalInterconnectUris')
         if not uris:
-            if 'logicalInterconnectNames' not in data:
-                raise HPOneViewValueError(SAS_LOGICAL_INTERCONNECT_NO_OPTIONS_PROVIDED)
+            if 'logicalInterconnectNames' not in self.data:
+                raise HPOneViewValueError(self.MSG_NO_OPTIONS_PROVIDED)
 
-            uris = self.__resolve_log_interconnect_names(data['logicalInterconnectNames'])
+            uris = self.__resolve_log_interconnect_names(self.data['logicalInterconnectNames'])
 
-        self.oneview_client.sas_logical_interconnects.update_compliance_all(uris)
-        return True, SAS_LOGICAL_INTERCONNECT_CONSISTENT, {}
-
-    def __get_log_interconnect_by_name(self, name):
-        resource = self.oneview_client.sas_logical_interconnects.get_by("name", name)
-        if resource:
-            return resource[0]
-        else:
-            return None
+        self.resource_client.update_compliance_all(uris)
+        return True, self.MSG_CONSISTENT, {}
 
     def __resolve_log_interconnect_names(self, interconnectNames):
         uris = []
         for name in interconnectNames:
-            li = self.__get_log_interconnect_by_name(name)
+            li = self.get_by_name(name)
             if not li:
-                raise HPOneViewResourceNotFound(SAS_LOGICAL_INTERCONNECT_NOT_FOUND)
+                raise HPOneViewResourceNotFound(self.MSG_NOT_FOUND)
             uris.append(li['uri'])
 
         return uris
 
-    def __update_firmware(self, uri, data):
-        options = data['firmware'].copy()
+    def __update_firmware(self, uri):
+        options = self.data['firmware'].copy()
         if 'sppName' in options:
             options['sppUri'] = '/rest/firmware-drivers/' + options.pop('sppName')
 
-        firmware = self.oneview_client.sas_logical_interconnects.update_firmware(options, uri)
+        firmware = self.resource_client.update_firmware(options, uri)
 
-        return True, SAS_LOGICAL_INTERCONNECT_FIRMWARE_UPDATED, dict(li_firmware=firmware)
+        return True, self.MSG_FIRMWARE_UPDATED, dict(li_firmware=firmware)
 
     def __update_configuration(self, uri):
-        result = self.oneview_client.sas_logical_interconnects.update_configuration(uri)
+        result = self.resource_client.update_configuration(uri)
 
-        return True, SAS_LOGICAL_INTERCONNECT_CONFIGURATION_UPDATED, dict(sas_logical_interconnect=result)
+        return True, self.MSG_CONFIGURATION_UPDATED, dict(sas_logical_interconnect=result)
 
-    def __replace_drive_enclosure(self, uri, data):
-        result = self.oneview_client.sas_logical_interconnects.replace_drive_enclosure(
-            data['replace_drive_enclosure'],
+    def __replace_drive_enclosure(self, uri):
+        result = self.resource_client.replace_drive_enclosure(
+            self.data['replace_drive_enclosure'],
             uri)
 
-        return True, SAS_LOGICAL_INTERCONNECT_DRIVE_ENCLOSURE_REPLACED, dict(sas_logical_interconnect=result)
+        return True, self.MSG_DRIVE_ENCLOSURE_REPLACED, dict(sas_logical_interconnect=result)
 
 
 def main():
