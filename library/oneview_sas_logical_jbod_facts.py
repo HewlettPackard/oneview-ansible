ff6acc08c488134bde2b6565fd909a3a143b2a00
Mariana Kreisig <mariana.kreisig@hpe.com>
2017-04-12 10:44:28 -0300

    Refactoring SAS Logical JBODs modules

--- library/oneview_sas_logical_jbod_facts.py
+++ library/oneview_sas_logical_jbod_facts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 ###
-# Copyright (2016) Hewlett Packard Enterprise Development LP
+# Copyright (2016-2017) Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
@@ -15,21 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###
-
-from ansible.module_utils.basic import *
-try:
-    from hpOneView.oneview_client import OneViewClient
-    from hpOneView.common import transform_list_to_dict
-    from hpOneView.exceptions import HPOneViewException
-
-    HAS_HPE_ONEVIEW = True
-except ImportError:
-    HAS_HPE_ONEVIEW = False
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'curated'}
 
 DOCUMENTATION = '''
 ---
 module: oneview_sas_logical_jbod_facts
 short_description: Retrieve facts about one or more of the OneView SAS Logical JBODs.
+version_added: "2.3"
 description:
     - Retrieve facts about one or more of the SAS Logical JBODs from OneView.
 requirements:
@@ -37,21 +31,6 @@ requirements:
     - "hpOneView >= 3.0.0"
 author: "Mariana Kreisig (@marikrg)"
 options:
-    config:
-      description:
-        - Path to a .json configuration file containing the OneView client configuration.
-          The configuration file is optional. If the file path is not provided, the configuration will be loaded from
-          environment variables.
-      required: false
-    params:
-      description:
-        - List of params to delimit, filter and sort the list of resources.
-        - "params allowed:
-          'start': The first item to return, using 0-based indexing.
-          'count': The number of resources to return.
-          'filter': A general filter/query string to narrow the list of items returned.
-          'sort': The sort order of the returned data set."
-      required: false
     name:
       description:
         - Name of SAS Logical JBODs.
@@ -59,50 +38,50 @@ options:
     options:
       description:
         - "List with options to gather additional facts about SAS Logical JBODs and related resources.
-          Options allowed: drives."
+          Options allowed: C(drives)."
       required: false
 notes:
-    - "A sample configuration file for the config parameter can be found at:
-       https://github.hpe.com/Rainforest/oneview-ansible/blob/master/examples/oneview_config.json"
-    - "Check how to use environment variables for configuration at:
-       https://github.com/HewlettPackard/oneview-ansible#environment-variables"
     - This resource is only available on HPE Synergy
+
+extends_documentation_fragment:
+    - oneview
+    - oneview.factsparams
 '''
 
 EXAMPLES = '''
 - name: Gather facts about all SAS Logical JBODs
-    oneview_sas_logical_jbod_facts:
-    config: "{{ config_path }}"
+  oneview_sas_logical_jbod_facts:
+    config: "{{ config }}"
 
 - debug: var=sas_logical_jbods
 
 - name: Gather paginated, filtered and sorted facts about SAS Logical JBODs
-    oneview_sas_logical_jbod_facts:
-      config: "{{ config }}"
-      params:
-        start: 0
-        count: 2
-        sort: 'name:descending'
-        filter: "state='Configured'"
+  oneview_sas_logical_jbod_facts:
+    config: "{{ config }}"
+    params:
+      start: 0
+      count: 2
+      sort: 'name:descending'
+      filter: "state='Configured'"
 
 - debug: var=sas_logical_jbods
 
 - name: Gather facts about a SAS Logical JBOD by name
-    oneview_sas_logical_jbod_facts:
+  oneview_sas_logical_jbod_facts:
     config: "{{ config_path }}"
     name: "Name of the SAS Logical JBOD"
 
 - debug: var=sas_logical_jbods
 
 - name: Gather facts about a SAS Logical JBOD by name, with the list of drives allocated
-    oneview_sas_logical_jbod_facts:
+  oneview_sas_logical_jbod_facts:
     config: "{{ config }}"
     name: "{{ sas_logical_jbod_name }}"
     options:
       - drives
 
-    - debug: var=sas_logical_jbods
-    - debug: var=sas_logical_jbod_drives
+- debug: var=sas_logical_jbods
+- debug: var=sas_logical_jbod_drives
 '''
 
 RETURN = '''
@@ -116,57 +95,43 @@ sas_logical_jbod_drives:
     returned: Always, but can be null.
     type: complex
 '''
-HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
 
 
-class SasLogicalJbodFactsModule(object):
-    argument_spec = dict(
-        config=dict(required=False, type='str'),
-        name=dict(required=False, type='str'),
-        options=dict(required=False, type='list'),
-        params=dict(required=False, type='dict'),
-    )
+from ansible.module_utils.basic import AnsibleModule
+from module_utils.oneview import OneViewModuleBase
 
+
+class SasLogicalJbodFactsModule(OneViewModuleBase):
     def __init__(self):
-        self.module = AnsibleModule(argument_spec=self.argument_spec,
-                                    supports_check_mode=False)
+        argument_spec = dict(
+            name=dict(required=False, type='str'),
+            options=dict(required=False, type='list'),
+            params=dict(required=False, type='dict'),
+        )
+        super(SasLogicalJbodFactsModule, self).__init__(additional_arg_spec=argument_spec)
 
-        if not HAS_HPE_ONEVIEW:
-            self.module.fail_json(msg=HPE_ONEVIEW_SDK_REQUIRED)
+    def execute_module(self):
+        ansible_facts = {}
 
-        if not self.module.params['config']:
-            self.oneview_client = OneViewClient.from_environment_variables()
+        if self.module.params['name']:
+            name = self.module.params['name']
+            sas_logical_jbods = self.oneview_client.sas_logical_jbods.get_by('name', name)
+
+            if self.module.params.get('options') and sas_logical_jbods:
+                ansible_facts = self.__gather_optional_facts(self.module.params['options'], sas_logical_jbods[0])
         else:
-            self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+            sas_logical_jbods = self.oneview_client.sas_logical_jbods.get_all(**self.facts_params)
 
-    def run(self):
-        try:
-            ansible_facts = {}
+        ansible_facts['sas_logical_jbods'] = sas_logical_jbods
 
-            if self.module.params['name']:
-                name = self.module.params['name']
-                sas_logical_jbods = self.oneview_client.sas_logical_jbods.get_by('name', name)
-
-                if self.module.params.get('options') and sas_logical_jbods:
-                    ansible_facts = self.__gather_optional_facts(self.module.params['options'], sas_logical_jbods[0])
-            else:
-                params = self.module.params.get('params') or {}
-                sas_logical_jbods = self.oneview_client.sas_logical_jbods.get_all(**params)
-
-            ansible_facts['sas_logical_jbods'] = sas_logical_jbods
-
-            self.module.exit_json(changed=False,
-                                  ansible_facts=ansible_facts)
-
-        except HPOneViewException as exception:
-            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
+        return dict(changed=False,
+                    ansible_facts=ansible_facts)
 
     def __gather_optional_facts(self, options, sas_logical_jbod):
-        options = transform_list_to_dict(options)
         ansible_facts = {}
         sas_logical_jbods_client = self.oneview_client.sas_logical_jbods
 
-        if options.get('drives'):
+        if self.options.get('drives'):
             ansible_facts['sas_logical_jbod_drives'] = sas_logical_jbods_client.get_drives(sas_logical_jbod['uri'])
 
         return ansible_facts
