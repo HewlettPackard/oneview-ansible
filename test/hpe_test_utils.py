622a32ae56923b5a0ea2f9f27fb8ecf0ef135805
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-12-06 16:15:06 -0300

    Adds Base classes for unit tests using pytest style

--- test/hpe_test_utils.py
+++ test/hpe_test_utils.py
@@ -17,10 +17,87 @@
 ###
 
 import importlib
+import pytest
+import re
 import yaml
-from ansible.compat.tests import unittest, mock
+
+from ansible.compat.tests import mock
 from oneview_module_loader import ONEVIEW_MODULE_UTILS_PATH
 from hpOneView.oneview_client import OneViewClient
+
+
+class OneViewBaseTest(object):
+    @pytest.fixture(autouse=True)
+    def setUp(self, mock_ansible_module, mock_ov_client, request):
+        class_name = type(self).__name__
+        marker = request.node.get_marker('resource')
+        self.resource = getattr(mock_ov_client, "%s" % (marker.kwargs[class_name]))
+        self.mock_ov_client = mock_ov_client
+        self.mock_ansible_module = mock_ansible_module
+
+    @pytest.fixture
+    def testing_module(self):
+        resource_name = type(self).__name__.replace('Test', '')
+        resource_module_path_name = self.underscore(resource_name.replace('Module', ''))
+
+        testing_module = importlib.import_module('library.' + resource_module_path_name)
+        self.testing_class = getattr(testing_module, resource_name)
+        try:
+            # Load scenarios from module examples (Also checks if it is a valid yaml)
+            self.EXAMPLES = yaml.load(testing_module.EXAMPLES, yaml.SafeLoader)
+
+        except yaml.scanner.ScannerError:
+            message = "Something went wrong while parsing yaml from {}.EXAMPLES".format(self.testing_class.__module__)
+            raise Exception(message)
+        return testing_module
+
+    def underscore(self, word):
+        word = re.findall('[A-Z][^A-Z]*', word)
+        word = 'oneview_' + str.join('_', word).lower()
+        return word
+
+    def test_main_function_should_call_run_method(self, testing_module, mock_ansible_module):
+        mock_ansible_module.params = {'config': 'config.json'}
+
+        main_func = getattr(testing_module, 'main')
+
+        with mock.patch.object(self.testing_class, "run") as mock_run:
+            main_func()
+            mock_run.assert_called_once()
+
+
+class OneViewBaseFactsTest(OneViewBaseTest):
+    def test_should_get_all_using_filters(self, testing_module):
+        self.resource.get_all.return_value = []
+
+        params_get_all_with_filters = dict(
+            config='config.json',
+            name=None,
+            params={
+                'start': 1,
+                'count': 3,
+                'sort': 'name:descending',
+                'filter': 'purpose=General',
+                'query': 'imported eq true'
+            })
+        self.mock_ansible_module.params = params_get_all_with_filters
+
+        self.testing_class().run()
+
+        self.resource.get_all.assert_called_once_with(start=1, count=3, sort='name:descending', filter='purpose=General', query='imported eq true')
+
+    def test_should_get_all_without_params(self, testing_module):
+        self.resource.get_all.return_value = []
+
+        params_get_all_with_filters = dict(
+            config='config.json',
+            name=None
+        )
+        self.mock_ansible_module.params = params_get_all_with_filters
+
+        self.testing_class().run()
+
+        self.resource.get_all.assert_called_once_with()
 
 
 class OneViewBaseTestCase(object):
@@ -55,7 +132,7 @@ class OneViewBaseTestCase(object):
         self.mock_ansible_module = mock.Mock()
         mock_ansible_module.return_value = self.mock_ansible_module
 
-        self.__set_module_examples()
+        self._set_module_examples()
 
     def test_main_function_should_call_run_method(self):
         self.mock_ansible_module.params = {'config': 'config.json'}
@@ -66,7 +143,7 @@ class OneViewBaseTestCase(object):
             main_func()
             mock_run.assert_called_once_with()
 
-    def __set_module_examples(self):
+    def _set_module_examples(self):
         self.testing_module = importlib.import_module(self.testing_class.__module__)
 
         try:
