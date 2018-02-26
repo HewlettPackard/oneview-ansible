fd6fe40203a5fd0dfcb2104c2c63256c591a239f
Sijeesh Kattumunda <mailtosije@gmail.com>
2018-02-25 23:20:13 -0700

    Added support for API version (server profile template)

--- test/test_oneview_server_profile_template.py
+++ test/test_oneview_server_profile_template.py
@@ -128,9 +128,8 @@ class TestServerProfileTemplateModule(OneViewBaseTest):
         expected = CREATED_BASIC_TEMPLATE.copy()
         expected.update(BASIC_TEMPLATE_MODIFIED)
 
-        self.resource.update.assert_called_once_with(
-            resource=expected, id_or_uri=expected["uri"]
-        )
+        self.resource.update.assert_called_once_with(resource=expected,
+                                                     id_or_uri=expected["uri"], force=True)
 
         self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
@@ -151,7 +150,8 @@ class TestServerProfileTemplateModule(OneViewBaseTest):
         expected = CREATED_BASIC_TEMPLATE.copy()
         expected.update(BASIC_TEMPLATE_MODIFIED)
 
-        self.resource.update.assert_called_once_with(resource=expected, id_or_uri=expected["uri"])
+        self.resource.update.assert_called_once_with(resource=expected,
+                                                     id_or_uri=expected["uri"], force=True)
 
         self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
@@ -192,7 +192,7 @@ class TestServerProfileTemplateModule(OneViewBaseTest):
 
         ServerProfileTemplateModule().run()
 
-        self.resource.delete.assert_called_once_with(CREATED_BASIC_TEMPLATE)
+        self.resource.delete.assert_called_once_with(CREATED_BASIC_TEMPLATE, force=True)
 
         self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
