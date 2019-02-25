1d691d570c38767c3b756fcfac92267bf4bf0129
sijeesh <mailtosije@gmail.com>
2019-02-25 12:28:42 +0530

    Cleaned example files

--- library/oneview_fc_network_facts.py
+++ library/oneview_fc_network_facts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 ###
-# Copyright (2018-2019) Hewlett Packard Enterprise Development LP
+# Copyright (2016-2019) Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
