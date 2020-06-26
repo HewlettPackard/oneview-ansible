94cbeb5db9e053d503653d034a086f3fc2ca959c
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-06-26 12:59:01 +0530

    Fix UTs

--- test/test_oneview_rack.py
+++ test/test_oneview_rack.py
@@ -36,8 +36,8 @@ UPDATED_RACK_TEMPLATE = dict(
     newName='Rename Rack',
     autoLoginRedistribution=True,
     fabricType='FabricAttach',
-    rackMounts=[{'mountUri': '/rest/server-hardware/31393736-3831-4753-567h-30335837524E', 'topUSlot': 'fakeip'},
-                {'mountUri': '/rest/server-hardware/31393736-3831-4753-567h-30335837526F', 'topUSlot': 'fakeip'}] 
+    rackMounts=[{'mountUri': '/rest/server-hardware/31393736-3831-4753-567h-30335837524E', 'topUSlot': 20},
+                {'mountUri': '/rest/server-hardware/31393736-3831-4753-567h-30335837526F', 'topUSlot': 20}] 
 )
 
 UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS = dict(
@@ -45,8 +45,7 @@ UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS = dict(
     newName='Rename Rack',
     autoLoginRedistribution=True,
     fabricType='FabricAttach',
-    rackMounts=[{'mountUri': '/rest/server-hardware/31393736-3831-4753-569h-30335837524E', 'topUSlot': 'fakeip'},
-                {'mountUri': '/rest/server-hardware/31393736-3831-4753-568h-30335837526F', 'topUSlot': 'fakeip'}] 
+    rackMounts=[{'mountUri': '/rest/server-hardware/31393736-3831-4753-568h-30335837526F', 'topUSlot': 20}] 
 )
 
 PARAMS_FOR_PRESENT = dict(
@@ -103,12 +102,12 @@ class TestRackModule(OneViewBaseTest):
 
     def test_update_when_data_has_modified_attributes_in_mountUris(self):
         data_merged = DEFAULT_RACK_TEMPLATE.copy()
-
+        DEFAULT_RACK_TEMPLATE['rackMounts'] = { 'mountUri': '/rest/server-hardware/31393736-3831-4753-569h-30335837524E', 'topUSlot': 20 }
         data_merged['name'] = 'Rename Rack'
 
         self.resource.update.return_value = data_merged
 
-        self.resource.data = UPDATED_RACK_TEMPLATE
+        self.resource.data = DEFAULT_RACK_TEMPLATE
         self.mock_ansible_module.params = PARAMS_WITH_CHANGES
 
         RackModule().run()
@@ -121,12 +120,12 @@ class TestRackModule(OneViewBaseTest):
 
     def test_update_when_data_has_modified_attributes_not_in_mountUris(self):
         data_merged = DEFAULT_RACK_TEMPLATE.copy()
-
+        DEFAULT_RACK_TEMPLATE['rackMounts'] = { 'mountUri': '/rest/server-hardware/31393736-3831-4753-568h-30335837526F', 'topUSlot': 20 }
         data_merged['name'] = 'Rename Rack'
 
         self.resource.update.return_value = data_merged
 
-        self.resource.data = UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS
+        self.resource.data = DEFAULT_RACK_TEMPLATE
         self.mock_ansible_module.params = PARAMS_WITH_CHANGES
 
         RackModule().run()
