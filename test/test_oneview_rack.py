c80666d1c1674fec39eb5a32a4d54b65b17dd411
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-06-30 12:28:34 +0530

    fix UTs

--- test/test_oneview_rack.py
+++ test/test_oneview_rack.py
@@ -60,6 +60,12 @@ PARAMS_WITH_CHANGES = dict(
     data=UPDATED_RACK_TEMPLATE
 )
 
+PARAMS_WITH_MOUNTURI = dict(
+    config='config.json',
+    state='present',
+    data=UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS
+)
+
 PARAMS_FOR_ABSENT = dict(
     config='config.json',
     state='absent',
@@ -100,33 +106,13 @@ class TestRackModule(OneViewBaseTest):
             ansible_facts=dict(rack=DEFAULT_RACK_TEMPLATE)
         )
 
-    def test_update_when_data_has_modified_attributes_in_mountUris(self):
-        data_merged = DEFAULT_RACK_TEMPLATE.copy()
-        DEFAULT_RACK_TEMPLATE['rackMounts'] = [{'mountUri': '/rest/server-hardware/31393736-3831-4753-569h-30335837524E', 'topUSlot': 20}]
-        data_merged['name'] = 'Rename Rack'
-
-        self.resource.update.return_value = data_merged
-        self.data = DEFAULT_RACK_TEMPLATE
-        self.resource.data = DEFAULT_RACK_TEMPLATE
-        self.mock_ansible_module.params = PARAMS_WITH_CHANGES
-
-        RackModule().run()
-
-        self.mock_ansible_module.exit_json.assert_called_once_with(
-            changed=True,
-            msg=RackModule.MSG_UPDATED,
-            ansible_facts=dict(rack=data_merged)
-        )
-
     def test_update_when_data_has_modified_attributes_not_in_mountUris(self):
-        data_merged = DEFAULT_RACK_TEMPLATE.copy()
-        DEFAULT_RACK_TEMPLATE['rackMounts'] = [{'mountUri': '/rest/server-hardware/31393736-3831-4753-568h-30335837526F', 'topUSlot': 20}]
-        data_merged['name'] = 'Rename Rack'
+        self.resource.get_by.return_value = [UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS]
+        data_merged = UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS.copy()
 
         self.resource.update.return_value = data_merged
-        self.data = UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS
-        self.resource.data = DEFAULT_RACK_TEMPLATE
-        self.mock_ansible_module.params = PARAMS_WITH_CHANGES
+        self.resource.data = UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS
+        self.mock_ansible_module.params = PARAMS_WITH_MOUNTURI
 
         RackModule().run()
 
