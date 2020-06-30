68040e62bac0603a3ec0f4a7bb39f29b46729c09
Chebrolu <bala-sai-harika.chebrolu@hpe.com>
2020-06-30 13:20:18 +0530

    fix UTs

--- test/test_oneview_rack.py
+++ test/test_oneview_rack.py
@@ -107,12 +107,11 @@ class TestRackModule(OneViewBaseTest):
         )
 
     def test_update_when_data_has_modified_attributes_with_different_mountUris(self):
-        data_merged = DEFAULT_RACK_TEMPLATE.copy()
-        DEFAULT_RACK_TEMPLATE['rackMounts'] = [{'mountUri': '/rest/server-hardware/31393736-3831-4753-569h-30335837524E', 'topUSlot': 20}]
+        data_merged = UPDATED_RACK_TEMPLATE.copy()
         data_merged['name'] = 'Rename Rack'
 
         self.resource.update.return_value = data_merged
-        self.resource.data = DEFAULT_RACK_TEMPLATE
+        self.resource.data = UPDATED_RACK_TEMPLATE
         self.resource.current_resource = UPDATED_RACK_TEMPLATE_WITH_DIFFERENT_MOUNTURIS
         self.mock_ansible_module.params = PARAMS_WITH_CHANGES
 
