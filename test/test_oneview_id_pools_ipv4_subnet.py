befb3909b678bf6f6c452fba7be8b6546eef3c01
yuvirani <yuvaranisrec@gamil.com>
2021-03-03 12:38:35 +0530

    test with get_all

--- test/test_oneview_id_pools_ipv4_subnet.py
+++ test/test_oneview_id_pools_ipv4_subnet.py
@@ -77,7 +77,7 @@ class TestIdPoolsIpv4SubnetModule(OneViewBaseTest):
     OneViewBaseTestCase provides the mocks used in this test case
     """
     def test_should_create_new_id_pools_ipv4_subnet(self):
-        self.resource.get_by_name.return_value = None
+        self.resource.get_all.return_value = []
         self.resource.create.return_value = self.resource
         self.resource.data = DEFAULT_SUBNET_TEMPLATE
 
@@ -106,7 +106,7 @@ class TestIdPoolsIpv4SubnetModule(OneViewBaseTest):
 
     def test_should_get_the_same_resource_by_name(self):
         self.resource.data = DEFAULT_SUBNET_TEMPLATE
-        self.resource.get_by_name.return_value = self.resource
+        self.resource.get_all.return_value = self.resource
 
         self.mock_ansible_module.params = PARAMS_FOR_PRESENT
 
@@ -141,7 +141,7 @@ class TestIdPoolsIpv4SubnetModule(OneViewBaseTest):
 
         data_merged['count'] = 2
         self.resource.data = data_merged
-        self.resource.get_by_name.return_value = self.resource
+        self.resource.get_all.return_value = self.resource
         self.resource.allocate.return_value = {'idList': ['172.9.0.1', '172.9.0.2']}
 
         self.mock_ansible_module.params = PARAMS_FOR_ALLOCATE
@@ -158,7 +158,7 @@ class TestIdPoolsIpv4SubnetModule(OneViewBaseTest):
         data_merged = DEFAULT_SUBNET_TEMPLATE.copy()
         data_merged['idList'] = ['10.1.1.1', '10.1.1.2']
         self.resource.data = data_merged
-        self.resource.get_by_name.return_value = self.resource
+        self.resource.get_all.return_value = self.resource
         self.resource.collect.return_value = {'idList': ['10.1.1.1', '10.1.1.1']}
 
         self.mock_ansible_module.params = PARAMS_FOR_COLLECT
@@ -185,7 +185,7 @@ class TestIdPoolsIpv4SubnetModule(OneViewBaseTest):
         )
 
     def test_should_do_nothing_when_id_pools_ipv4_subnet_not_exist(self):
-        self.resource.get_by_name.return_value = None
+        self.resource.get_all.return_value = None
 
         self.mock_ansible_module.params = PARAMS_FOR_ABSENT
         self.mock_ansible_module.check_mode = True
